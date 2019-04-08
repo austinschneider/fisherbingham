1c916ad65a993ecc99a23ffc0ccfeeb7841456c7
Austin Schneider <hogenshpogen@gmail.com>
2019-04-07 22:06:57 -0500

    Corrected some typos. Added a test. Squeezed some dimensions that are automatically handled in R

--- MLE.py
+++ MLE.py
@@ -18,7 +18,7 @@ def FBLLH(theta, gamma, O, A, B, n, method='hg', withvol=False):
     #sorted_theta = theta[sorting_indices]
     #sorted_gamma = gamma[sorting_indices]
 
-    alpha = np.concatenate(theta, gamma)
+    alpha = np.concatenate((theta, gamma))
 
     if method == 'hg':
         c_res = fb.hgm_FB_2(alpha, withvol=withvol)
@@ -27,7 +27,8 @@ def FBLLH(theta, gamma, O, A, B, n, method='hg', withvol=False):
     elif method == 'MC':
         c_res = fb.G_FB(alpha, method='MC', withvol=withvol)
     elif method == 'SPA':
-        c_res = fb.saddleapprox_FB_revised(sorted(alpha[:p])+1.0, M=alpha[p:p*2]/2.0, dub=1, order=3) * np.exp(1)
+        #c_res = fb.saddleapprox_FB_revised(np.array(sorted(alpha[:p]))+1.0, M=alpha[p:p*2]/2.0, dub=1, order=3) * np.exp(1)
+        c_res = fb.SPA(alpha, withvol=withvol)
     elif method == 'lpinv':
         raise ValueError('Not sure what lpinv is supposed to do.')
         c_res = lpinv(alpha)
@@ -36,9 +37,14 @@ def FBLLH(theta, gamma, O, A, B, n, method='hg', withvol=False):
 
     c = c_res[0]
 
-    l = -n*np.log(c) - np.sum(A.dot(O.T).dot(np.diag(theta)).dot(O) + gamma.dot(B.T).dot(O))
+    #l = -n*np.log(c) - np.sum(A.dot(O.T).dot(np.diag(theta)).dot(O) + gamma.dot(B.T).dot(O))
+    l = -n*np.log(c) - np.trace(O.dot(B).dot(gamma[:,None].T) + A.dot(O.T).dot(np.diag(theta)).dot(O))
 
-    grad = -n*c_res[1:] / c + np.concatenate((-np.diag(O.dot(A).dot(O.T)),O.dot(B)))
+    #grad = -n*c_res[1:] / c + np.concatenate((-np.diag(O.dot(A).dot(O.T)),O.dot(B)))
+    theta_grad = np.diag(O.dot(A).dot(A.T))
+    gamma_grad = np.squeeze(B.T.dot(O.T))
+    param_grad = np.concatenate((theta_grad, gamma_grad))
+    grad = -n*c_res[1:] / c + param_grad
 
     return l, grad
 
@@ -60,9 +66,9 @@ def grad_mle_update(theta, gamma, O, A, B, n, method='hg', withvol=False, min_st
 
     # try stepping once
     delta = 1
-    new_theta = theta + delta*grad[:,p]
+    new_theta = theta + delta*grad[:p]
     new_theta[0] = 0
-    new_gamma = gamma + delta*grad[p,:]
+    new_gamma = gamma + delta*grad[p:]
     new_l, new_grad = FBLLH(new_theta, new_gamma, O, A, B, n, method=method, withvol=withvol)
 
     # try some smaller steps
@@ -77,20 +83,20 @@ def grad_mle_update(theta, gamma, O, A, B, n, method='hg', withvol=False, min_st
 
 def grad_mle_orth_update(theta, gamma, O, A, B, n, method='hg', withvol=False, tol=1e-3):
     m_theta = np.diag(theta)
-    AA = m_theta.dot(O).dot(A).dot(O.T).dot(m_theta) + gamma.dot(B.T).dot(O.T)
+    #AA = m_theta.dot(O).dot(A).dot(O.T).dot(m_theta) + gamma.dot(B.T).dot(O.T)
+    AA = m_theta.dot(O).dot(A).dot(O.T) - O.dot(A).dot(O.T).dot(m_theta) + gamma.dot(np.squeeze(B.T)) * O.T
     vhat = AA - AA.T
-    cc = -np.sum(inp.diag(A.dot(O.T).dot(m_theta).dot(O) + gamma.dot(B.T).dot(O.T)))
+    #cc = -np.sum(np.diag(A.dot(O.T).dot(m_theta).dot(O) + gamma.dot(B.T).dot(O.T)))
     l, grad = FBLLH(theta, gamma, O, A, B, n, method=method, withvol=withvol)
     delta = np.sign(np.sum(AA.dot(vhat)))
-    new_O = expm.expm(vhat*delta).dot(O)
-    new_cc = -np.sum(np.diag(A.dot(new_O.T).dot(m_theta).dot(new_O) + gamma.dot(B.T).dot(new_O.T)))
+    new_O = expm.expm_Higham08(vhat*delta).dot(O)
     new_l, new_grad = FBLLH(theta, gamma, new_O, A, B, n, method=method, withvol=withvol)
 
-    f0 = lambda t0: -FBLLH(theta, gamma, expm.expm(vhat*t0).dot(O), A, B, n, method=method, withvol=withvol)[0]
+    f0 = lambda t0: -FBLLH(theta, gamma, expm.expm_Higham08(vhat*t0).dot(O), A, B, n, method=method, withvol=withvol)[0]
 
-    res = scipy.optimize.minimize_scalar(f0, bound=[-100.,100.])
+    res = scipy.optimize.minimize_scalar(f0, bounds=[-100.,100.])
 
-    new_O_1 = expm.expm(vhat*res.fun).dot(O)
+    new_O_1 = expm.expm_Higham08(vhat*res.fun).dot(O)
 
     new_l_1, new_grad_1 = FBLLH(theta, gamma, new_O_1, A, B, n, method=method, withvol=withvol)
 
@@ -122,7 +128,7 @@ def grad_mle_update_optim_sqrt(theta, gamma, O, A, B, n, method='hg', withvol=Fa
         new_y = y*(1.0+dl * grad[p:p*2])
         return -FBLLH(new_x**2.0, new_x**2.0, O, A, B, n, method=method, withvol=withvol)[0]
 
-    res = scipy.optimize.minimize_scalar(f, bound=[0.0, dmax])
+    res = scipy.optimize.minimize_scalar(f, bounds=[0.0, dmax])
 
     delta = res.fun
     new_x = x*(1.0+delta * grad[:p])
@@ -130,17 +136,17 @@ def grad_mle_update_optim_sqrt(theta, gamma, O, A, B, n, method='hg', withvol=Fa
     new_theta = new_x**2.0
     new_gamma = new_y**2.0
 
-    new_l, new_grad = FBLLH(new_theta, new_gamma, O, A, B, n, method=method, withvol=withvol)[0]
+    new_l, new_grad = FBLLH(new_theta, new_gamma, O, A, B, n, method=method, withvol=withvol)
 
     return new_theta, new_gamma, new_l, new_grad, delta
 
-def MLE(A, B, n, theta=None, gamma=None, O=None, method='hg', withvol=False, max_iter=200):
+def MLE(A, B, n, theta=None, gamma=None, O=None, method='hg', withvol=False, max_iter=200, tol=1e-3):
 
     p = A.shape[0]
 
     # get the initial estimates for theta, gamma, O if they are not provided
     if theta is None:
-        theta = np.range(p)
+        theta = np.arange(p)
     theta = theta - np.amin(theta)
     assert(np.all(theta >= 0))
     if gamma is None:
@@ -151,7 +157,7 @@ def MLE(A, B, n, theta=None, gamma=None, O=None, method='hg', withvol=False, max
     # try a first update of gamma and theta with n=1 for some reason?
     theta, gamma, l, grad, delta = grad_mle_update(theta, gamma, O, A, B, 1, withvol=withvol)
     # try a second update with n=n now?
-    new_theta, new_gamma, new_l, new_grad, new_delta = grad_mle_update(theta, gamma, O, A, B, n, withvol=withvol)
+    new_theta, new_gamma, new_l, new_grad, new_delta = grad_mle_update(theta, gamma, O, A, B, n, method=method, withvol=withvol)
 
     a = scipy.linalg.norm(theta - new_theta, ord=1)
     b = scipy.linalg.norm(gamma - new_gamma, ord=1)
@@ -162,10 +168,34 @@ def MLE(A, B, n, theta=None, gamma=None, O=None, method='hg', withvol=False, max
         b = scipy.linalg.norm(gamma - new_gamma, ord=1)
         theta, gamma, l, grad, delta = new_theta, new_gamma, new_l, new_grad, new_delta
 
-        O, _, _, _ = grad_mle_orth_update(theta, gamma, O, A, B, n)
-        new_theta, new_gamma, new_l, new_grad, new_delta = grad_mle_update_optim_sqrt(theta, gamma, O, A, B, n, withvol=withvol)
+        O, _, _, _ = grad_mle_orth_update(theta, gamma, O, A, B, n, method=method)
+        new_theta, new_gamma, new_l, new_grad, new_delta = grad_mle_update_optim_sqrt(theta, gamma, O, A, B, n, method=method, withvol=withvol)
 
         i += 1
 
     return {'theta':new_theta, 'gamma':new_gamma, 'O':O, 'l':new_l, 'grad':new_grad}
+
+def test():
+    # astronomy data test
+    A = np.zeros((3,3))
+    A[0,0]=0.3119
+    A[0,1]=0.0292/2.0; A[1,0]=A[0,1]
+    A[0,2]=0.0707/2.0;A[2,0]=A[0,2]
+    A[1,1]=0.3605
+    A[1,2]=0.0462/2.0; A[2,1]=A[1,2]
+    A[2,2]=0.3276
+    A = 2.0*A - np.diag(np.diag(A))
+
+    B = np.array([-0.0063,-0.0054,-0.0762])[:,None]
+    B = -B
+
+    res = MLE(A, B, n=1, theta=None, gamma=None, O=None, method='hg', withvol=True, max_iter=200)
+
+    print res
+
+    res = MLE(A, B, n=1, theta=None, gamma=None, O=None, method='MC', withvol=True, max_iter=200)
+
+    print res
+
+test()
 
